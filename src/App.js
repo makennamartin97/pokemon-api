fb4e0b27f04ec6a4f71d7a459d53d76618e1dcbf
makennamartin97 <makennamartin97@yahoo.com>
2020-07-15 17:33:05 -0400

    files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokelist, setpokelist] = useState([]);
+  const [search, setsearch] = useState("");
+  const [display, setdisplay] = useState([]);
+
+  const getpokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+      .then(response => {
+        //console.log(response.data.results);
+        //setpokelist(response.data.results);
+        return response.json();
+    }).then(response => {
+        setpokelist(response.results);
+        setdisplay(response.results);
+        
+    })
+    .catch(err => console.log(err));
+  }
+  useEffect( () => {
+    getpokemon();
+  }, []);
+
+  useEffect( () => {
+    setdisplay(pokelist.filter(a => a.name.includes(search)))
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="jumbotron bg-danger text-light text-center lead">Pokemon</div>
+      <div className="text-center ">
+        <button className="btn btn-primary btn-lg mt-4" onClick={getpokemon}>Fetch Pokemon</button>
+        <div className="form-group text-center">
+          <label forHtml="searchpoke" className="text-dark lead mt-3">Search:</label>
+          <input type="text" name="searchpoke" className="form-control " onChange={e => setsearch(e.target.value)}/>
+        </div>
+      </div>
+      <ul className="list-group text-center mt-5">
+        { display.map((poke, i) => 
+          <li key={i} className="list-group-item">{poke.name}</li>
+        
+        )}
+      </ul>
+
+      
     </div>
   );
 }
